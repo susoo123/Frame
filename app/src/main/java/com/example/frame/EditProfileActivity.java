3bba2f768c4cf2c2835d0dc9caf69aeb4614ab5d
susoo123 <suyeon613@gmail.com>
2021-08-10 22:54:28 +0900

    내정보 수정 기능 완성 (프로필 이미지 변경 하는 것 까지!!) -8월10일-

--- app/src/main/java/com/example/frame/EditProfileActivity.java
+++ app/src/main/java/com/example/frame/EditProfileActivity.java
@@ -1,13 +1,20 @@
 package com.example.frame;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
@@ -24,6 +31,7 @@ import com.android.volley.toolbox.StringRequest;
 
 import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.util.Util;
 import com.example.frame.etc.SessionManager;
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -34,20 +42,26 @@ import org.json.JSONObject;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.Base64;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 import com.example.frame.etc.SessionManager;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 
 public class EditProfileActivity extends AppCompatActivity {
 
 private TextInputEditText email,name,birth,phone_num;
 private TextView btn_edit_profile;
-private ImageButton btn_photo_upload;
+private ImageButton btn_watching_photo;
 private static String URL_Edit_profile = "http://ec2-52-79-204-252.ap-northeast-2.compute.amazonaws.com/edit_profile.php";
 private static String URL_read_profile = "http://ec2-52-79-204-252.ap-northeast-2.compute.amazonaws.com/read_profile.php";
 
@@ -56,13 +70,14 @@ SessionManager sessionManager;
 private Bitmap bitmap;
 CircleImageView profile_image;
 ImageView photo;
+String encodeImageString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
 
-        btn_photo_upload = findViewById(R.id.btn_photo_profile);
+        btn_watching_photo = findViewById(R.id.btn_photo_profile);
         //profile_image.findViewById(R.id.imageView3);
         photo = findViewById(R.id.imageView33);
 
@@ -73,10 +88,12 @@ ImageView photo;
         phone_num = findViewById(R.id.til_phone);
         btn_edit_profile =findViewById(R.id.btn_edit_profile);
 
+
         btn_edit_profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Edit_profile();
+                //upload_img();
             }
         });
 
@@ -86,21 +103,40 @@ ImageView photo;
         //Log.e("soo","쉐어드 확인: "+mEmail);
 
         // Glide로 이미지 표시하기
-        String imageUrl2 = " http://www.culture.go.kr/upload/rdf/21/03/rdf_2021033110325407198.jpg";
-        String imageUrl = "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory&fname=https://k.kakaocdn.net/dn/EShJF/btquPLT192D/SRxSvXqcWjHRTju3kHcOQK/img.png";
-        Glide.with(this).load(imageUrl2).into(photo);
+        //Glide.with(this).load().into(photo);
 
 
-
-
-
-
-        btn_photo_upload.setOnClickListener(new View.OnClickListener() {
+        //이미지 업로드를 위해 갤러리로 가는 버튼?
+        btn_watching_photo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Dexter.withContext(EditProfileActivity.this)
+                        .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                        .withListener(new PermissionListener() {
+                            @Override
+                            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+
+                                Intent intent = new Intent(Intent.ACTION_PICK);
+                                intent.setType("image/*");
+                                intent.setAction(Intent.ACTION_GET_CONTENT);
+                                launcher.launch(intent);
+                            }
+
+                            @Override
+                            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                            }
+
+                            @Override
+                            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                                permissionToken.continuePermissionRequest();
+                            }
+                        }).check();
 
             }
         });
+
+
 
         //디비에 저장된 값을 가져오기
         StringRequest stringRequest = new StringRequest(Request.Method.POST,URL_read_profile,
@@ -128,10 +164,8 @@ ImageView photo;
                                     birth.setText(db_birth);
                                     phone_num.setText(db_phone_num);
 
+
                                 }
-
-
-
 
 
                             }else {
@@ -153,9 +187,6 @@ ImageView photo;
                     @Override
                     public void onErrorResponse(com.android.volley.VolleyError error) {
 
-
-
-
                         Toast.makeText(EditProfileActivity.this, "프로필 에러2" + error.toString(),Toast.LENGTH_SHORT).show();
 
                     }
@@ -170,7 +201,6 @@ ImageView photo;
 
                 params.put("email",sEmail);
 
-
                 return params;
             }
         };
@@ -180,6 +210,60 @@ ImageView photo;
 
     }
 
+
+
+    //이미지 업로드 관련
+    ActivityResultLauncher<Intent> launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>()
+            {
+                @Override
+                public void onActivityResult(ActivityResult result)
+                {
+                    if (result.getResultCode() == RESULT_OK)
+                    {
+                        Intent intent = result.getData();
+                        Uri filepath = intent.getData();
+
+//                      imageview.setImageURI(uri);
+
+                        Glide.with(EditProfileActivity.this)
+                                .load(filepath)
+                                .centerCrop()
+                                .into(photo);
+
+                        try{
+
+                            InputStream inputStream = getContentResolver().openInputStream(filepath);
+                            bitmap = BitmapFactory.decodeStream(inputStream);
+                            photo.setImageBitmap(bitmap);
+                            encodeBitmapImage(bitmap);
+                        }catch (Exception e){
+
+                        }
+
+
+
+                    }
+                }
+            });
+
+
+
+    //이미지 인코딩
+    private void encodeBitmapImage(Bitmap bitmap) {
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100,byteArrayOutputStream);
+
+        byte[] bytesofimage = byteArrayOutputStream.toByteArray();
+        encodeImageString = android.util.Base64.encodeToString(bytesofimage, Base64.DEFAULT);
+
+        Log.e("soo", "encodeImageString : " +encodeImageString);
+    }
+
+
+
+    //내 정보 수정하기 메소드
     private void Edit_profile() {
 
         //내정보 수정 액티비티에서 editText에 적힌 텍스트들을 스트링으로 바꾼다.
@@ -204,8 +288,9 @@ ImageView photo;
                             }
 
                         } catch (JSONException e) {
+
                             e.printStackTrace();
-                            Toast.makeText(EditProfileActivity.this, "회원가입 에러" + e.toString(),Toast.LENGTH_SHORT).show();
+                            Toast.makeText(EditProfileActivity.this, "회원가입 수정 에러" + e.toString(),Toast.LENGTH_SHORT).show();
 
                         }
                     }
@@ -213,8 +298,8 @@ ImageView photo;
                 new Response.ErrorListener() {//에러 콜백
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(EditProfileActivity.this, "에러" + error.toString(),Toast.LENGTH_SHORT).show();
 
+                        Toast.makeText(EditProfileActivity.this, "에러" + error.toString(),Toast.LENGTH_SHORT).show();
 
                     }
                 })
@@ -231,6 +316,7 @@ ImageView photo;
                 params.put("email",email);
                 params.put("birth",birth);
                 params.put("phone_num",phone_num);
+                params.put("upload",encodeImageString);
 
                 return params;
             }
@@ -247,45 +333,9 @@ ImageView photo;
 
 
 
-    private void chooseFile(){
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent,"Select picture"),1);
-
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if(requestCode==1 && resultCode == RESULT_OK && data != null && data.getData() != null){
-            Uri filePath = data.getData();
-            try {
-                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),filePath);
-                profile_image.setImageBitmap(bitmap);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            uploadPicture(getStringImage(bitmap));
-        }
-    }
-
-    private void uploadPicture(final String photo) {
 
 
-    }
-    public String getStringImage(Bitmap bitmap){
 
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
-
-        byte[] imageByteArray = byteArrayOutputStream.toByteArray();
-        //String encodeImage = Base64.encodeToString(imageByteArray,Base64.DEFAULT);
-
-        return null; //encodeImage;
-    }
 
 
 }
